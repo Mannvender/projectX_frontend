9111884e7c611bb36f9f1f7c3715407ac6760982
RoBo_ <mannvender001@gmail.com>
2018-01-26 18:20:06 +0530

    colour picker trial

--- imgProcessing.js
+++ imgProcessing.js
@@ -110,19 +110,52 @@ $(function () {
     }
 });
 
-// function for loading different views of t-shirt
+// function for loading different views of t-shirt and colours
 $(function () {
     let tshirtBlock = $('#tshirt-block');
     let frontViewBtn = $('#frontViewBtn');
     let backViewBtn = $('#backViewBtn');
 
+    let whiteBtn = $('#white-btn');
+    let blueBtn = $('#blue-btn');
+    let redBtn = $('#red-btn');
+    let greenBtn = $('#green-btn');
+    let blackBtn = $('#black-btn');
+    let yellowBtn = $('#yellow-btn');
+    let greyBtn = $('#grey-btn');
+
+
     frontViewBtn.click(function () {
         tshirtBlock.css('background-image', 'url(./img/tshirt_front.jpg)');
-    })
+    });
 
     backViewBtn.click(function () {
-        tshirtBlock.css('background-image', 'url(./img/tshirt_back.jpg)');
-    })
+        tshirtBlock.css('background-image', 'url(./img/png_back.png)');
+    });
+
+    whiteBtn.click(function () {
+        console.log("whitebtn fired");
+        tshirtBlock.css('background-color', 'white');
+    });
+    blueBtn.click(function () {
+        console.log("bluebtn fired");
+        tshirtBlock.css('background-color', 'blue');
+    });
+    redBtn.click(function () {
+        tshirtBlock.css('background-color', 'red');
+    });
+    greenBtn.click(function () {
+        tshirtBlock.css('background-color', 'green');
+    });
+    blackBtn.click(function () {
+        tshirtBlock.css('background-color', 'black');
+    });
+    yellowBtn.click(function () {
+        tshirtBlock.css('background-color', 'yellow');
+    });
+    greyBtn.click(function () {
+        tshirtBlock.css('background-color', 'grey');
+    });
 
 
 });
