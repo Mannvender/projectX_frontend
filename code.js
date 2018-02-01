e0a845ddc7cf48448a95330c1a77a03421951441
Ashish Kuamr <entropy.ash@gmail.com>
2018-02-01 15:56:56 +0530

    smooth resize and drag + code cleanup

--- code.js
+++ code.js
@@ -1,124 +1,39 @@
 let input_front = document.getElementById('img_input_front');
-let designArea_front = document.getElementById('design_area_front');
+let designArea_front = $('#design_area_front');
 let input_back = document.getElementById('img_input_back');
-let designArea_back = document.getElementById('design_area_back');
+let designArea_back = $('#design_area_back');
 let costDiv = document.getElementById('cost_div');
-
-let initialX;
-let initialY;
-let imageToBeMoved;
-
-let images_front;
-let images_back;
 
 let cost = 299;
 
-input_front.addEventListener('change', addImageFront);
-input_back.addEventListener('change', addImageBack);
+input_front.addEventListener('change', () => addImage(true));
+input_back.addEventListener('change', () => addImage(false));
 
-function addImageFront() {
+function addImage(front) {
     cost += 50;
     updateCost();
     let image = input_front.files[0];
     let reader = new FileReader();
     reader.readAsDataURL(image);
     reader.onloadend = () => {
-        let newImageDiv = document.createElement('div');
-        newImageDiv.className = 'images_front';
-        newImageDiv.onmousedown = prepareToMoveImage;
-        newImageDiv.onmouseup = stopMovingImage;
-        newImageDiv.style.backgroundImage = 'url(' + reader.result + ')';
-        designArea_front.appendChild(newImageDiv);
-        images_front = $(".images_front");
-        setResizable(images_front);
-        addCloseBtn(images_front, 'front');
+        let newImageDiv = $(`<div class="images_front" style="background-image: url(${reader.result})">
+</div>`);
+        setDragableAndResizable($(newImageDiv));
+        if (front === true) {
+            designArea_front.append(newImageDiv);
+        } else {
+            designArea_back.append(newImageDiv);
+        }
     }
 }
 
-function addImageBack() {
-    cost += 50;
-    updateCost();
-    let image = input_back.files[0];
-    let reader = new FileReader();
-    reader.readAsDataURL(image);
-    reader.onloadend = () => {
-        let newImageDiv = document.createElement('div');
-        newImageDiv.className = 'images_back';
-        newImageDiv.onmousedown = prepareToMoveImage;
-        newImageDiv.onmouseup = stopMovingImage;
-        newImageDiv.style.backgroundImage = 'url(' + reader.result + ')';
-        designArea_back.appendChild(newImageDiv);
-        images_back = $(".images_back");
-        setResizable(images_back);
-        addCloseBtn(images_back, 'back');
-    }
-}
-
-function setResizable(jqueryElement) {
-    jqueryElement.resizable();
-}
-
-
-function prepareToMoveImage(event) {
-    initialX = event.pageX - event.target.offsetLeft;
-    initialY = event.pageY - event.target.offsetTop;
-    imageToBeMoved = event.target;
-    event.target.style.cursor = 'move';
-    window.addEventListener('mousemove', moveImage);
-}
-
-function moveImage(event) {
-    let posX = event.pageX - initialX;
-    let posY = event.pageY - initialY;
-    imageToBeMoved.style.left = posX + 'px';
-    imageToBeMoved.style.top = posY + 'px';
-}
-
-function stopMovingImage() {
-    window.removeEventListener('mousemove', moveImage);
-    event.target.style.cursor = 'default';
-    // alignImageInsideDesignArea();
-}
-
-function alignImageInsideDesignArea() {
-    let widthOfDesignArea = parseInt(window.getComputedStyle(designArea).width);
-    let widthOfImage = parseInt(window.getComputedStyle(imageToBeMoved).width);
-    let heightOfDesignArea = parseInt(window.getComputedStyle(designArea).height);
-    let heightOfImage = parseInt(window.getComputedStyle(imageToBeMoved).height);
-
-    let minimumLeft = 0;
-    let minimumTop = 0;
-    let maximumLeft = widthOfDesignArea - widthOfImage;
-    let maximumTop = heightOfDesignArea - heightOfImage;
-
-    let currentLeft = parseInt(window.getComputedStyle(imageToBeMoved).left);
-    let currentTop = parseInt(window.getComputedStyle(imageToBeMoved).top);
-
-    let reAdjustedLeft;
-    let reAdjustedTop;
-
-    if (currentLeft < minimumLeft) {
-        reAdjustedLeft = minimumLeft;
-    } else if (currentLeft > maximumLeft) {
-        reAdjustedLeft = maximumLeft;
-    } else {
-        reAdjustedLeft = currentLeft;
-    }
-
-    if (currentTop < minimumTop) {
-        reAdjustedTop = minimumTop;
-    } else if (currentTop > maximumTop) {
-        reAdjustedTop = maximumTop;
-    } else {
-        reAdjustedTop = currentTop;
-    }
-
-    $(imageToBeMoved).animate({left: reAdjustedLeft, top: reAdjustedTop});
+function setDragableAndResizable(jqueryElement) {
+    jqueryElement.draggable().resizable();
 }
 
 function addCloseBtn(JQueryElement, FrontOrBack) {
     // append CloseButton to last element of JQueryElement it get
-    var b = $('<button type="button" class="close" aria-label="Close">\n' +
+    let b = $('<button type="button" class="close" aria-label="Close">\n' +
         '  <span aria-hidden="true">&times;</span>\n' +
         '</button>');
     JQueryElement.last().append(b);
