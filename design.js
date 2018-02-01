57181db06eb381b293d6370f1acf876be863e278
Ashish Kuamr <entropy.ash@gmail.com>
2018-02-01 17:50:26 +0530

    Redesign delete button

--- design.js
+++ design.js
@@ -5,6 +5,7 @@ let designArea_back = $('#design_area_back');
 let costDiv = document.getElementById('cost_div');
 
 let cost = 299;
+let numberOfImagesAdded = 0;
 
 input_front.addEventListener('change', () => addImage(true));
 input_back.addEventListener('change', () => addImage(false));
@@ -16,37 +17,28 @@ function addImage(front) {
     let reader = new FileReader();
     reader.readAsDataURL(image);
     reader.onloadend = () => {
-        let newImageDiv = $(`<div class="images_front" style="background-image: url(${reader.result})">
-</div>`);
-        setDragableAndResizable($(newImageDiv));
+        let newImageId = numberOfImagesAdded + 1;
+        let newImageDiv = $(`<div class="images" data-id="${newImageId}" style="background-image: url(${reader.result})">
+                <span onclick="deleteImage(${newImageId})" class="floating-icon-container shadow delete-icon-container">
+                <i class="fa fa-trash floating-icon"></i>
+                </span>
+            </div>`);
         if (front === true) {
             designArea_front.append(newImageDiv);
         } else {
             designArea_back.append(newImageDiv);
         }
-    }
+        numberOfImagesAdded++;
+        setDragableAndResizable($(newImageDiv));
+    };
+}
+
+function deleteImage(imageId) {
+    $(`[data-id='${imageId}']`).remove();
 }
 
 function setDragableAndResizable(jqueryElement) {
     jqueryElement.draggable().resizable();
-}
-
-function addCloseBtn(JQueryElement, FrontOrBack) {
-    // append CloseButton to last element of JQueryElement it get
-    let b = $('<button type="button" class="close" aria-label="Close">\n' +
-        '  <span aria-hidden="true">&times;</span>\n' +
-        '</button>');
-    JQueryElement.last().append(b);
-    b.click(() => {
-        cost -= 50;
-        updateCost();
-        JQueryElement.last().remove();
-        if (FrontOrBack === 'front') {
-            input_front.value = '';
-        } else {
-            input_back.value = '';
-        }
-    })
 }
 
 function updateCost() {
