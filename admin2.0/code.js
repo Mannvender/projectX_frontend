99aa1833a2010cc939f630fa7ced90a5110943f2
RoBo_ <mannvender001@gmail.com>
2018-03-11 17:06:35 +0530

    /adminPanel - basic layout set

---
+++ admin2.0/code.js
@@ -0,0 +1,20 @@
+class Selectize {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        let initValue;
+        $('.action-box').selectric({
+            onInit: function (element) {
+                initValue = $(this).val();
+            },
+            onChange: function (element) {
+                if ($(this).val() !== initValue)
+                    $(element).parents('form').submit();
+            }
+        });
+    }
+}
+
+new Selectize();
