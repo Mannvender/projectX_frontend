843dab62a298a51857204693b6a9081ce34c5b75
RoBo_ <mannvender001@gmail.com>
2018-03-26 22:59:11 +0530

    navBar fixing

--- tshirt-costomize/save_design.js
+++ tshirt-costomize/save_design.js
@@ -58,5 +58,16 @@ function sendDesign() {
     document.getElementById('design_form').submit();
 }
 
+function openInNewTab(link) {
+    let win = window.open('' + link, '_blank');
+    if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+    } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+    }
+}
+
 
 
